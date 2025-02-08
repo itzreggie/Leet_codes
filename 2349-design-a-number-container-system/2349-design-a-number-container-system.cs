40ac94dbf802fa55672b122a896cdc8f4ac9c6b8
Reggie <Rejoice.ekong@student.lut.fi>
2025-02-08 07:56:12 +0200

    Time: 170 ms (50%), Space: 175 MB (25%) - LeetHub

---
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+
+public class NumberContainers {
+    private Dictionary<int, int> indexToNumber;
+    private Dictionary<int, SortedSet<int>> numberToIndices;
+
+    public NumberContainers() {
+        indexToNumber = new Dictionary<int, int>();
+        numberToIndices = new Dictionary<int, SortedSet<int>>();
+    }
+
+    public void Change(int index, int number) {
+        if (indexToNumber.ContainsKey(index)) {
+            int oldNumber = indexToNumber[index];
+            if (numberToIndices.ContainsKey(oldNumber)) {
+                numberToIndices[oldNumber].Remove(index);
+                if (numberToIndices[oldNumber].Count == 0) {
+                    numberToIndices.Remove(oldNumber);
+                }
+            }
+        }
+
+        indexToNumber[index] = number;
+        if (!numberToIndices.ContainsKey(number)) {
+            numberToIndices[number] = new SortedSet<int>();
+        }
+        numberToIndices[number].Add(index);
+    }
+
+    public int Find(int number) {
+        if (numberToIndices.ContainsKey(number) && numberToIndices[number].Count > 0) {
+            return numberToIndices[number].Min;
+        }
+        return -1;
+    }
+}
