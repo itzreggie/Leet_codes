ef616169ec0d521cad4f60b7608f5efa8445b78f
Reggie <Rejoice.ekong@student.lut.fi>
2025-02-09 09:40:25 +0200

    Time: 58 ms (68.75%), Space: 61.8 MB (100%) - LeetHub

---
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public long CountBadPairs(int[] nums) {
+        int n = nums.Length;
+        long totalPairs = (long)n * (n - 1) / 2;
+        Dictionary<int, int> freqMap = new Dictionary<int, int>();
+        
+        foreach (var key in nums.Select((val, idx) => val - idx)) {
+            if (freqMap.ContainsKey(key)) {
+                freqMap[key]++;
+            } else {
+                freqMap[key] = 1;
+            }
+        }
+        
+        long goodPairs = 0;
+        foreach (int count in freqMap.Values) {
+            goodPairs += (long)count * (count - 1) / 2;
+        }
+        
+        return totalPairs - goodPairs;
+    }
+}
