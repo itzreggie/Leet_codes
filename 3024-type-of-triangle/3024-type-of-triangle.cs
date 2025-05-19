721b9655e1a28a2eaa21d19321d2b2decea48102
Reggie <Rejoice.ekong@student.lut.fi>
2025-05-19 11:26:55 +0300

    Time: 0 ms (100%), Space: 45 MB (15.63%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.cs
@@ -0,0 +1,20 @@
+
+public class Solution {
+    public string TriangleType(int[] nums) {
+        if (nums.Length != 3) return "none";
+
+        // Check triangle inequality condition
+        if (nums[0] + nums[1] <= nums[2] || nums[0] + nums[2] <= nums[1] || nums[1] + nums[2] <= nums[0]) {
+            return "none";
+        }
+
+        // Determine triangle type
+        if (nums[0] == nums[1] && nums[1] == nums[2]) {
+            return "equilateral";
+        } else if (nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2]) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
+    }
+}
