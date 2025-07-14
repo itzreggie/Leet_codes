d084f0b2538f24836ca5e72dbc0c2bf6d3b413b5
Reggie <itzreggie5678@gmail.com>
2025-07-14 17:32:32 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cs
@@ -0,0 +1,21 @@
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public int GetDecimalValue(ListNode head) {
+        int result = 0;
+
+        while (head != null) {
+            result = (result << 1) | head.val;
+            head = head.next;
+        }
+
+        return result;
+    }
+}
