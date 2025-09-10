6fa53389dc35090334c9e3f87abf7aa082e60d3f
Reggie <Rejoice.ekong@student.lut.fi>
2025-09-10 17:58:20 +0300

    Time: 0 ms (100%), Space: 40 MB (26.32%) - LeetHub

---
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.cs
@@ -0,0 +1,21 @@
+
+
+public class Solution {
+    public int[] GetNoZeroIntegers(int n) {
+        for (int a = 1; a < n; a++) {
+            int b = n - a;
+            if (IsNoZero(a) && IsNoZero(b)) {
+                return new int[] { a, b };
+            }
+        }
+        return new int[] { }; // Should never hit this due to problem constraints
+    }
+
+    private bool IsNoZero(int num) {
+        while (num > 0) {
+            if (num % 10 == 0) return false;
+            num /= 10;
+        }
+        return true;
+    }
+}
