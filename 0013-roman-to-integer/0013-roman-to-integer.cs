f95c10d5eb1457ea4664f1b0731af0ad0757b285
Reggie <Rejoice.ekong@student.lut.fi>
2025-10-30 23:55:52 +0200

    Time: 3 ms (71.55%), Space: 50.4 MB (68.77%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int RomanToInt(string s) {
+        Dictionary<char, int> romanMap = new Dictionary<char, int> {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int total = 0;
+        for (int i = 0; i < s.Length; i++) {
+            int currentVal = romanMap[s[i]];
+            int nextVal = (i + 1 < s.Length) ? romanMap[s[i + 1]] : 0;
+
+            if (currentVal < nextVal) {
+                total -= currentVal;
+            } else {
+                total += currentVal;
+            }
+        }
+
+        return total;
+    }
+}
