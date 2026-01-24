54b50c2d9bcb19302c7fb75053a3126becb8b6f6
Reggie <Rejoice.ekong@student.lut.fi>
2026-01-24 13:38:22 +0200

    Time: 599 ms (18.18%), Space: 66.4 MB (15.91%) - LeetHub

---
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int MinPairSum(int[] nums) {
+        Array.Sort(nums);
+        int left = 0;
+        int right = nums.Length - 1;
+        int maxPairSum = 0;
+
+        while (left < right) {
+            int pairSum = nums[left] + nums[right];
+            if (pairSum > maxPairSum)
+                maxPairSum = pairSum;
+
+            left++;
+            right--;
+        }
+
+        return maxPairSum;
+    }
+}
