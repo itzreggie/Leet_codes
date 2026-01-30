9ccb9d39e9574b36b6a387c24de33a67f689b3fa
Reggie <Rejoice.ekong@student.lut.fi>
2026-01-30 11:40:56 +0200

    Time: 1 ms (100%), Space: 61.3 MB (94.7%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.Length; i++) {
+            // Either extend the current subarray or start a new one
+            currentSum = Math.Max(nums[i], currentSum + nums[i]);
+            maxSum = Math.Max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
