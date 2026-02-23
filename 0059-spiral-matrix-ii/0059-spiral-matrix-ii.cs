f19d4b5676ac1701458c06157778468943ee8bda
Reggie <Rejoice.ekong@student.lut.fi>
2026-02-23 21:12:38 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cs
@@ -0,0 +1,41 @@
+public int[][] GenerateMatrix(int n)
+{
+    int[][] matrix = new int[n][];
+    for (int i = 0; i < n; i++)
+        matrix[i] = new int[n];
+
+    int left = 0, right = n - 1;
+    int top = 0, bottom = n - 1;
+    int num = 1;
+
+    while (left <= right && top <= bottom)
+    {
+        // Fill top row
+        for (int j = left; j <= right; j++)
+            matrix[top][j] = num++;
+        top++;
+
+        // Fill right column
+        for (int i = top; i <= bottom; i++)
+            matrix[i][right] = num++;
+        right--;
+
+        // Fill bottom row
+        if (top <= bottom)
+        {
+            for (int j = right; j >= left; j--)
+                matrix[bottom][j] = num++;
+            bottom--;
+        }
+
+        // Fill left column
+        if (left <= right)
+        {
+            for (int i = bottom; i >= top; i--)
+                matrix[i][left] = num++;
+            left++;
+        }
+    }
+
+    return matrix;
+}
