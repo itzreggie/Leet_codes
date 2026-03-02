91f37f0ba4b5a75f90501d24e8461ce23bf07438
Reggie <Rejoice.ekong@student.lut.fi>
2026-03-02 11:18:18 +0200

    Time: 156 ms (44.41%), Space: 55.7 MB (96.3%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+public class Solution {
+    public int FindKthLargest(int[] nums, int k) {
+        PriorityQueue<int, int> minHeap = new PriorityQueue<int, int>();
+
+        foreach (int num in nums) {
+            minHeap.Enqueue(num, num);
+
+            if (minHeap.Count > k)
+                minHeap.Dequeue();
+        }
+
+        return minHeap.Peek();
+    }
+}
