37acfef73d3fd66bb23687494716c32c1952dbcd
Reggie <Rejoice.ekong@student.lut.fi>
2026-05-19 12:53:50 +0300

    Time: 0 ms (100%), Space: 67.4 MB (38.96%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int GetCommon(int[] nums1, int[] nums2) {
+        int i = 0, j = 0;
+
+        while (i < nums1.Length && j < nums2.Length) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            }
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return -1;
+    }
+}
