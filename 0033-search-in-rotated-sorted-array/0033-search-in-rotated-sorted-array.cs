575d98a95435ec39003e74a531d356535fd14579
Reggie <Rejoice.ekong@student.lut.fi>
2026-05-22 20:52:38 +0300

    Time: 0 ms (100%), Space: 42.3 MB (63.43%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            // Check if left half is sorted
+            if (nums[left] <= nums[mid]) {
+                // Target is in the left sorted half
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // Otherwise, right half is sorted
+            else {
+                // Target is in the right sorted half
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
